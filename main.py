c5ff59d3171748b9d81801c1b9c9e4d99959d3c0
Kop4444 <yugrinkd@mail.ru>
2024-04-20 19:54:37 +0300

    mp3

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog, QSlider
-from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaPlaylist, QMediaContent
 from PyQt5.QtCore import QUrl, QFileInfo, Qt, QTimer
+
 
 class MusicPlayer(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Музыкальный проигрыватель")
-        self.setGeometry(100, 100, 400, 250)  # Увеличиваем высоту виджета
+        self.setGeometry(100, 100, 400, 250)
 
         self.layout = QVBoxLayout()
 
         self.player = QMediaPlayer()
+        self.playlist = QMediaPlaylist()
+        self.player.setPlaylist(self.playlist)
+
         self.player.stateChanged.connect(self.print_state)
 
-        self.btn_select_file = QPushButton("Выбрать аудиофайл")
-        self.btn_select_file.clicked.connect(self.select_file)
-        self.layout.addWidget(self.btn_select_file)
+        self.btn_select_files = QPushButton("Выбрать аудиофайл(ы)")
+        self.btn_select_files.clicked.connect(self.select_files)
+        self.layout.addWidget(self.btn_select_files)
 
         self.btn_play = QPushButton("▶️")
         self.btn_play.clicked.connect(self.play)
@@ -37,6 +41,14 @@ class MusicPlayer(QWidget):
         self.btn_backward.clicked.connect(self.backward)
         self.layout.addWidget(self.btn_backward)
 
+        self.btn_next_track = QPushButton(">")
+        self.btn_next_track.clicked.connect(self.next_track)
+        self.layout.addWidget(self.btn_next_track)
+
+        self.btn_previous_track = QPushButton("<")
+        self.btn_previous_track.clicked.connect(self.previous_track)
+        self.layout.addWidget(self.btn_previous_track)
+
         self.slider_volume = QSlider(Qt.Horizontal)
         self.slider_volume.setMinimum(0)
         self.slider_volume.setMaximum(100)
@@ -47,7 +59,7 @@ class MusicPlayer(QWidget):
         self.layout.addWidget(self.slider_volume)
 
         self.label_status = QLabel()
-        self.label_status.setStyleSheet("font-size: 18px;")  # Увеличиваем размер надписи
+        self.label_status.setStyleSheet("font-size: 18px;")
         self.layout.addWidget(self.label_status)
 
         self.timer = QTimer(self)
@@ -55,14 +67,15 @@ class MusicPlayer(QWidget):
 
         self.setLayout(self.layout)
 
-    def select_file(self):
-        file_path, _ = QFileDialog.getOpenFileName(self, "Выберите аудиофайл", "", "Audio Files (*.mp3)")
-        if file_path:
-            if QFileInfo(file_path).exists():
-                self.player.setMedia(QMediaContent(QUrl.fromLocalFile(file_path)))
-                self.label_status.setText("Выбран файл: " + file_path)
-            else:
-                self.label_status.setText("Файл не найден")
+    def select_files(self):
+        file_paths, _ = QFileDialog.getOpenFileNames(self, "Выберите аудиофайлы", "", "Audio Files (*.mp3)")
+        if file_paths:
+            for file_path in file_paths:
+                if QFileInfo(file_path).exists():
+                    self.playlist.addMedia(QMediaContent(QUrl.fromLocalFile(file_path)))
+                    self.label_status.setText("Выбрано файлов: " + str(self.playlist.mediaCount()))
+                else:
+                    self.label_status.setText("Файлы не найдены")
 
     def play(self):
         if self.player.state() == QMediaPlayer.StoppedState:
@@ -80,6 +93,12 @@ class MusicPlayer(QWidget):
 
     def forward(self):
         self.player.setPosition(self.player.position() + 10000)
+
+    def previous_track(self):
+        self.playlist.previous()
+
+    def next_track(self):
+        self.playlist.next()
 
     def change_volume(self):
         volume = self.slider_volume.value()
